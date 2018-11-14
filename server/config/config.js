a167dd7c30aed9c8ee1faaa1f7df86c2268fe0da
Chiri <chirichignoa@gmail.com>
2018-11-13 23:30:07 -0300

    Port fix v2

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,5 @@ if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else if (env === 'production') {
-    process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://todoapp:todoapp123@ds161183.mlab.com:61183/todoapp';
 }
